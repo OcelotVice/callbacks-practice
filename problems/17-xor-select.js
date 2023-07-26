b9e411b7a2d3a0edfc8b7ab4953dad01be3d75fa
gabriel <iamgabrielvice@gmail.com>
2023-07-26 21:22:57 +1000

    second commit

---
+++ problems/17-xor-select.js
@@ -0,0 +1,53 @@
+/*******************************************************************************
+Write a function `xorSelect` that accepts an array and two callback as arguments.
+The function should return a new array containing elements of the original array
+that result in true when passed in one of the callbacks, but not both.
+
+Examples:
+
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+
+let isPositive = function(n) {
+  return n > 0;
+};
+
+console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+// [ -2, 1, 3 ]
+
+
+let longString = function(s) {
+  return s.length > 4;
+};
+
+let startsA = function(s) {
+  return s[0] === "a";
+};
+
+console.log(
+  xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+);
+// [ 'art', 'app', 'buttery' ]
+*******************************************************************************/
+
+let xorSelect = function(array, cb1,cb2) {
+  let new_array = [];
+
+  array.forEach(function(ele){
+    let bool1 = cb1(ele);
+    let bool2 = cb2(ele);
+
+    if (bool1 ^ bool2) new_array.push(ele);
+  })
+
+  return new_array;
+};
+
+
+
+
+
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = xorSelect;
